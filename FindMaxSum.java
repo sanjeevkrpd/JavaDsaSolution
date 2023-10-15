99839d74722539f7e63fcedcdbd5bf943617d6a4
Sanjeev kumar <Sanjeevkrpd11@gmail.com>
2023-10-15 23:14:52 +0530

    Find Maximum Sum of sub array using naive approch

---
+++ FindMaxSum.java
@@ -0,0 +1,23 @@
+package JavaDsaSolution;
+
+public class FindMaxSum {
+    public static void main(String[] args) {
+        int[] arr = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+
+        int maxSum=0;
+
+        for (int i = 0; i < arr.length; i++) {
+            
+            int sum=0;
+            for (int j = i; j < arr.length; j++) {
+                
+                sum+=arr[j];
+
+                if(sum>maxSum){
+                    maxSum=sum;
+                }
+            }
+        }
+        System.out.println(maxSum);
+    }
+}
